07fbd4882e6e595d11207c4a804702b89189c7ec
smiwind <huong.trinh@themartec.com>
2025-05-15 16:05:41 +0700

    fix for env

--- app/workflows/employer_branding_company/utils/tools.py
+++ app/workflows/employer_branding_company/utils/tools.py
@@ -1,20 +1,22 @@
 from langchain_core.tools import tool
 import requests
+
+from app.workflows.employer_branding_mvp.utils.tools import get_content_from_url
 from settings import settings
 from app.helpers.rag_core import query_documents
 
-
-@tool
-def get_content_from_url(raw_url: str):
-    """Always use this function to crawl the content when user input the URL"""
-    url = f"https://r.jina.ai/{raw_url}"
-    headers = {
-        "Authorization": f'Bearer {settings.JINA_API_KEY}'
-    }
-
-    response = requests.get(url, headers=headers)
-    
-    return response.text
+#
+# @tool
+# def get_content_from_url(raw_url: str):
+#     """Always use this function to crawl the content when user input the URL"""
+#     url = f"https://r.jina.ai/{raw_url}"
+#     headers = {
+#         "Authorization": f'Bearer {settings.JINA_API_KEY}'
+#     }
+#
+#     response = requests.get(url, headers=headers)
+#
+#     return response.text
 
 @tool
 def get_content_from_library(request: str):
