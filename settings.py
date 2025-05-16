46ee54be40076a08ac49e85870fd7cb6b5ca4e35
smiwind <huong.trinh@themartec.com>
2025-05-16 10:07:34 +0700

    debug on cloud deploymeny

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
     JINA_API_KEY: str
-    LANGSMITH_API_KEY: str
+    # LANGSMITH_API_KEY: str
     AUTHEN_TOKEN: str
 
     model_config = SettingsConfigDict(env_file=('.env', '.test.env', '.dev.env'), env_file_encoding='utf-8',
