7b98e6ea8e293c78126c1e9f22004d8a284f4c4b
smiwind <huong.trinh@themartec.com>
2025-05-16 09:54:26 +0700

    debug on cloud deploymeny

--- settings.py
+++ settings.py
@@ -9,14 +9,18 @@ class Settings(BaseSettings):
     LANGSMITH_API_KEY: str
     AUTHEN_TOKEN: str
 
-    model_config = SettingsConfigDict(env_file=('.env', '.test.env', '.dev.env'), env_file_encoding='utf-8', extra='ignore')
+    model_config = SettingsConfigDict(env_file=('.env', '.test.env', '.dev.env'), env_file_encoding='utf-8',
+                                      extra='ignore')
 
 
 @lru_cache
 def get_settings():
     build_env: str = os.getenv("BUILD_ENVIRONMENT", "dev")
     print(f"BUILD_ENVIRONMENT: {build_env}")
-    settings = Settings(_env_file=f'environment/.{build_env}.env', _env_file_encoding='utf-8')
+    if "devCloud" in build_env:
+        settings = Settings(_env_file=f'.env', _env_file_encoding='utf-8')
+    else:
+        settings = Settings(_env_file=f'environment/.{build_env}.env', _env_file_encoding='utf-8')
     return settings
 
 
